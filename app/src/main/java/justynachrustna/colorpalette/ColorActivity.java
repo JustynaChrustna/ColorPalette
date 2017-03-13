06d7e5cdf6aa02a17779e0353d59bcff2f7aad46
Justyna Chrustna <justyna.chrustna@gmail.com>
2017-03-13 23:04:15 +0100

    Generator kodów: ButterKnife

--- app/src/main/java/justynachrustna/colorpalette/ColorActivity.java
+++ app/src/main/java/justynachrustna/colorpalette/ColorActivity.java
@@ -1,31 +1,59 @@
 package justynachrustna.colorpalette;
 
+import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
 
-public class ColorActivity extends AppCompatActivity {
+import butterknife.BindView;
 
+public class ColorActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @BindView(R.id.redSeekBar)
+    SeekBar redSeekBar;
+    @BindView(R.id.greenSeekBar)
+    SeekBar greenSeekBar;
+    @BindView(R.id.blueSeekBar)
+    SeekBar blueSeekBar;
+    @BindView(R.id.generateButton)
+    Button generateButton;
+    @BindView(R.id.saveButton)
+    Button saveButton;
+    @BindView(R.id.colorLinearLayout)
+    LinearLayout colorLinearLayout;
     private ActionBar actionBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+        ButterKnife.bind(this);
+
         actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);//wyświetli się strzałka
         //obsługa kliknięcia na strzałkę
+        generateButton.setOnClickListener(this);
+
 
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(item.getItemId()==android.R.id.home){
+        if (item.getItemId() == android.R.id.home) {
             NavUtils.navigateUpFromSameTask(this);//przejście w górę
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
