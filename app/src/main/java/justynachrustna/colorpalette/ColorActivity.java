b9078fb10ea351327a130aee8f93d591ef6c3957
Justyna Chrustna <justyna.chrustna@gmail.com>
2017-03-14 01:14:49 +0100

    Generator kolorów: activity z możiwością generowania kolorów za pomocą przycisku i suwaków

--- app/src/main/java/justynachrustna/colorpalette/ColorActivity.java
+++ app/src/main/java/justynachrustna/colorpalette/ColorActivity.java
@@ -1,5 +1,6 @@
 package justynachrustna.colorpalette;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
@@ -10,10 +11,18 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 
+import java.util.Random;
+
+import butterknife.ButterKnife;
+
 import butterknife.BindView;
+import butterknife.OnClick;
 
-public class ColorActivity extends AppCompatActivity implements View.OnClickListener {
+public class ColorActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
 
+    public static final String RED = "red";
+    public static final String GREEN = "green";
+    public static final String BLUE = "blue";
     @BindView(R.id.redSeekBar)
     SeekBar redSeekBar;
     @BindView(R.id.greenSeekBar)
@@ -26,8 +35,13 @@ public class ColorActivity extends AppCompatActivity implements View.OnClickList
     Button saveButton;
     @BindView(R.id.colorLinearLayout)
     LinearLayout colorLinearLayout;
-    private ActionBar actionBar;
 
+    private int red;
+    private int green;
+    private int blue;
+
+    private ActionBar actionBar;
+    private Random random= new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +52,10 @@ public class ColorActivity extends AppCompatActivity implements View.OnClickList
         actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);//wyświetli się strzałka
         //obsługa kliknięcia na strzałkę
-        generateButton.setOnClickListener(this);
+       redSeekBar.setOnSeekBarChangeListener(this);
+        greenSeekBar.setOnSeekBarChangeListener(this);
+        blueSeekBar.setOnSeekBarChangeListener(this);
+
 
 
     }
@@ -51,9 +68,70 @@ public class ColorActivity extends AppCompatActivity implements View.OnClickList
         }
         return super.onOptionsItemSelected(item);
     }
+    @OnClick(R.id.generateButton)
+    public void generate(){
+        red=random.nextInt(256);
+        green=random.nextInt(256);
+        blue=random.nextInt(256);
+        redSeekBar.setProgress(red);
+        greenSeekBar.setProgress(green);
+        blueSeekBar.setProgress(blue);
+
+        updateBackgroundColor();
+
+    }
+
+    private void updateBackgroundColor() {
+        int color= Color.rgb(red, green, blue);
+        colorLinearLayout.setBackgroundColor(color);
+    }
+
+    @OnClick(R.id.saveButton)
+    public void save(){
+
+    }
+
 
     @Override
-    public void onClick(View view) {
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        switch(seekBar.getId()){
+            case R.id.redSeekBar:
+                red=progress;
+                break;
+            case R.id.greenSeekBar:
+                red=progress;
+                break;
+            case R.id.blueSeekBar:
+                red=progress;
+                break;
+        }
+        updateBackgroundColor();
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+        outState.putInt(RED, red);
+        outState.putInt(GREEN, green);
+        outState.putInt(BLUE, blue);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        red=savedInstanceState.getInt(RED);
+        green=savedInstanceState.getInt(GREEN);
+        blue=savedInstanceState.getInt(BLUE);
 
     }
 }
